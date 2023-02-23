364ae524d76ccc49e7f6d8fb214570ce6e3a265d
Gary Ma <gary.ma@csgi.com>
2023-02-23 23:13:39 +0200

    added Spring MVC mode

---
+++ src/main/java/com/gary/springboot/microservice/springboot_microservice/web/TourRatingController.java
@@ -0,0 +1,31 @@
+package com.gary.springboot.microservice.springboot_microservice.web;
+
+import com.gary.springboot.microservice.springboot_microservice.repo.TourRatingRepository;
+import com.gary.springboot.microservice.springboot_microservice.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Tour Rating Controller
+ *
+ * Created by Mary Ellen Bowman
+ */
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
+public class TourRatingController {
+    TourRatingRepository tourRatingRepository;
+    TourRepository tourRepository;
+
+    @Autowired
+    public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
+        this.tourRatingRepository = tourRatingRepository;
+        this.tourRepository = tourRepository;
+    }
+
+    protected TourRatingController() {
+
+    }
+
+
+}
