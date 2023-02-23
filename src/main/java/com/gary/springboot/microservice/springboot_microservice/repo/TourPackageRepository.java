5b710d8b9700bae4028edb4df2f8cb35d3dd5bd1
Gary Ma <gary.ma@csgi.com>
2023-02-23 15:53:00 +0200

    added HAL browser dependency for a better view of API document

--- src/main/java/com/gary/springboot/microservice/springboot_microservice/repo/TourPackageRepository.java
+++ src/main/java/com/gary/springboot/microservice/springboot_microservice/repo/TourPackageRepository.java
@@ -1,13 +1,37 @@
 package com.gary.springboot.microservice.springboot_microservice.repo;
 
-import com.gary.springboot.microservice.springboot_microservice.domain.Tour;
 import com.gary.springboot.microservice.springboot_microservice.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     Optional<TourPackage> findTourPackageByName(String tourPackageName);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/gary/springboot/microservice/springboot_microservice/repo/TourRepository.java
+++ src/main/java/com/gary/springboot/microservice/springboot_microservice/repo/TourRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -19,4 +20,28 @@ public interface TourRepository extends CrudRepository<Tour, Integer> {
     List<Tour> findByTourPackageCodeAndDifficultyAndRegionAndPriceIsLessThan(String code, Difficulty difficulty, Region region, Integer maxPrice);
 
     Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
